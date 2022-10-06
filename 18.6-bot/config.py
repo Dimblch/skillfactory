fa7615e932806c6ded8bface3234d2574d86ecc4
Dimblch <dimblch080@gmail.com>
2022-10-06 21:38:29 +0300

    Commit from PyCharm, 18.6.1

---
+++ 18.6-bot/config.py
@@ -0,0 +1,10 @@
+# SkillFactory QAP-89 задание 18.6.1
+#
+# Файл с токеном бота и списком валют
+
+TOKEN = 'past_your_telegram_bot_token_here'
+keys = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB',
+    }
---
+++ 18.6-bot/extensions.py
@@ -0,0 +1,37 @@
+# SkillFactory QAP-89 задание 18.6.1
+#
+# Файл с классами
+
+import requests
+import json
+from config import keys
+
+class APIException(Exception):
+    pass
+
+class get_price():
+    @staticmethod
+    def convert(base: str, quote: str, amount: str):
+        if quote == base:
+            raise APIException('А смысл конвертировать само в себя?')
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту "{base}"')
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Не удалось обработать валюту "{quote}"')
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f'Не удалось обработать количество "{amount}"')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={base_ticker}&tsyms={quote_ticker}')
+        rate = json.loads(r.content)[keys[quote]]
+        total = rate * amount
+
+        return total
